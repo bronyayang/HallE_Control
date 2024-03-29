71a93a3684c8c2330d4f3c348c31a6a5d426034d
Shijia Yang <60163639+bronyayang@users.noreply.github.com>
2024-03-29 00:42:20 -0700

    Update model_control_eval.sh

--- scripts/v1_5/model_control_eval.sh
+++ scripts/v1_5/model_control_eval.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0 python3 llava/eval/model_controller.py \
+CUDA_VISIBLE_DEVICES=0 python3 eval/model_controller.py \
             --model-path LLAVA_CONTROLLER_MODEL_PATH \
             --sigma 0 \
             --gt_file_path ./data/VisualGenome_task \
